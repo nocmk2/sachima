b6256fedf30a8bca1cc3f7f8c1fd7f1f768909bb
zhangmk <51670196@qq.com>
2019-12-11 15:27:48 +0800

    role

--- server/role.go
+++ server/role.go
@@ -9,9 +9,10 @@ func roleHandler(c *gin.Context) {
 	claims := jwt.ExtractClaims(c)
 	user, _ := c.Get(identityKey)
 	c.JSON(200, gin.H{
-		"userID":   claims[identityKey],
-		"userName": user.(*User).UserName,
-		"text":     "Hello World.",
-		"role":     "view",
+		"userID":        claims[identityKey],
+		"userName":      user.(*User).UserName,
+		"text":          "Hello World.",
+		"role":          "view",
+		"lastlogintime": "1111",
 	})
 }
